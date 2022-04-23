b3d93bfe6d76393d5d27d3aa66f93e6b683b4a93
bearlexxx <alexey.d84@gmail.com>
2022-04-23 18:28:55 +0300

    lesson_12

---
+++ deploy/backend/library/urls.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from graphene_django.views import GraphQLView
+from rest_framework import permissions
+from rest_framework.authtoken import views
+
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from authors.views import AuthorModelViewSet
+from todo.views import ProjectModelViewSet, TodoModelViewSet
+from users.views import UserCustomViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Project library",
+        default_version='0.1',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@lidrary.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
+router.register('users', UserCustomViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('project_todos', TodoModelViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
+]
+
